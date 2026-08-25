c45ac63f66888618f8ce43660b193ea4df1d4b0f
pjh456 <147148383@qq.com>
2026-08-25 16:42:37 +0800

    chore: 递归导入第三方库子模块

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
-int main() { std::cout << "Hello World!"; }
+#include <optional>
+#include <pjh_json/json.hpp>
+#include <pjh_platform.hpp>
+#include <pjh_result.hpp>
+#include <string>
+
+#include "pjh_platform/file_watcher.hpp"
+
+int main()
+{
+    auto pesult = pjh::result::Result<std::string, std::nullopt_t>::Ok(
+        "Hello World from PJH_RESULT!");
+    std::cout << pesult.unwrap() << std::endl;
+
+    pjh::json::Json pson("Hello World from PJH_JSON!");
+    std::cout << pson.as_string() << std::endl;
+
+    pjh::platform::FileEventKind::Created;
+    std::cout << "Hello World!" << std::endl;
+}
